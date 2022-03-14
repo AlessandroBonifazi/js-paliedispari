c293d798afb3cf37ca561c5116f11120127bc66f
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-03-14 14:38:29 +0100

    adding logic ex1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,2 +1,24 @@
 // Debug
 console.log('JS OK!');
+
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+const word = prompt('input a word');
+
+function checkPalindrome(string) {
+    const len = word.length;
+
+    for (let i = 0; i < len / 2; i++) {
+
+        // check if first and last string are same
+        if (word[i] !== word[len - 1 - i]) {
+            return 'It is not a palindrome';
+        }
+    }
+    return 'It is a palindrome';
+}
+
+const result = checkPalindrome(word);
+
+console.log(result);
